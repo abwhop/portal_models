c9b269b4959fb7bb450e9241d31026539ee90af3
Sergey Mokrov <mokrov_sa@nlmk.com>
2024-05-05 01:13:38 +0300

    B24MOB-4049 - поправил модель новости

--- models/news.go
+++ models/news.go
@@ -96,24 +96,24 @@ func (NewsDetail) TableName() string {
 }
 
 type NewsBrief struct {
-	Type          string    `gorm:"column:type;primaryKey" json:"type"`
-	Id            int       `gorm:"column:id;primaryKey" json:"id"`
-	IsFavorite    bool      `gorm:"column:isFavorite" json:"isFavorite"`
-	IsRubric      bool      `gorm:"column:isRubric" json:"isRubric"`
-	Title         string    `gorm:"column:title" json:"title"`
-	Description   string    `gorm:"column:description" json:"description"`
-	ImageUrl      string    `gorm:"column:imageUrl" json:"imageUrl"`
-	CanComment    bool      `gorm:"column:canComment" json:"canComment"`
-	IsLiked       bool      `gorm:"column:isLiked" json:"isLiked"`
-	CanLikes      bool      `gorm:"column:canLikes" json:"canLikes"`
-	CountComments int       `gorm:"column:countComments" json:"countComments"`
-	CountViews    int       `gorm:"column:countViews" json:"countViews"`
-	CountLikes    int       `gorm:"column:countLikes" json:"countLikes"`
-	DateCreated   int       `gorm:"column:date_created" json:"dateCreated"`
-	DateUpdated   int       `gorm:"column:date_updated" json:"dateUpdated"`
-	LogId         int       `gorm:"column:logId" json:"logId"`
-	Rubric        *RubricDB `gorm:"column:rubric" json:"rubric"`
-	Data          *PostDB   `gorm:"column:data"  json:"data"`
+	Type          string          `gorm:"column:type;primaryKey" json:"type"`
+	Id            int             `gorm:"column:id;primaryKey" json:"id"`
+	IsFavorite    bool            `gorm:"column:isFavorite" json:"isFavorite"`
+	IsRubric      bool            `gorm:"column:isRubric" json:"isRubric"`
+	Title         string          `gorm:"column:title" json:"title"`
+	Description   string          `gorm:"column:description" json:"description"`
+	ImageUrl      string          `gorm:"column:imageUrl" json:"imageUrl"`
+	CanComment    bool            `gorm:"column:canComment" json:"canComment"`
+	IsLiked       bool            `gorm:"column:isLiked" json:"isLiked"`
+	CanLikes      bool            `gorm:"column:canLikes" json:"canLikes"`
+	CountComments int             `gorm:"column:countComments" json:"countComments"`
+	CountViews    int             `gorm:"column:countViews" json:"countViews"`
+	CountLikes    int             `gorm:"column:countLikes" json:"countLikes"`
+	DateCreated   int             `gorm:"column:date_created" json:"dateCreated"`
+	DateUpdated   int             `gorm:"column:date_updated" json:"dateUpdated"`
+	LogId         int             `gorm:"column:logId" json:"logId"`
+	Rubric        *RubricDB       `gorm:"column:rubric" json:"rubric"`
+	Data          *RepostedPostDB `gorm:"column:data"  json:"data"`
 	//ChangeDate int `gorm:"column:change_date" json:"change_date"`
 
 	//Descriptions *ListOfDescriptionDB `gorm:"column:descriptions" json:"descriptions"`
